84af5b39a4ac08f81e0b3dfdb494d338d4d58ca7
sreya287 <sreyamerinsam2828@gmail.com>
2025-11-16 19:43:06 +0530

    Implement removeElement function in Python

---
+++ python/27-remove-element.py
@@ -0,0 +1,15 @@
+#remove element
+#language: python
+#difficulty: easy
+
+class Solution(object):
+    def removeElement(self, nums, val):
+        n=len(nums)
+        c=0
+        k=0
+        for i in range(n):
+            if nums[i]!=val:
+                nums[k]=nums[i]
+                k+=1
+    
+        return k
