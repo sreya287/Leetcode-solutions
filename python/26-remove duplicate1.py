8ea2c0f8302857f1036c1143ca38d1a6c8fbaf77
sreya287 <sreyamerinsam2828@gmail.com>
2025-11-16 19:44:25 +0530

    Create 26-remove duplicate1.py

---
+++ python/26-remove duplicate1.py
@@ -0,0 +1,16 @@
+#remove duplicates from sorted array
+#difficulty: easy
+#language: python
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        val=nums[0]
+        k=1
+        for i in range (1,len(nums)):
+            if nums[i]!=val:
+                val=nums[k]=nums[i]
+                k+=1
+        return k
