a29b8f0ba7099708989f92f81772b32c44674a4b
sreya287 <sreyamerinsam2828@gmail.com>
2025-11-16 19:50:13 +0530

    Implement rotate function for array rotation

---
+++ python/189-rotate-array.py
@@ -0,0 +1,13 @@
+#rotate array
+#difficulty: easy
+#language: python
+#link: https://leetcode.com/problems/rotate-array/
+class Solution(object):
+    def rotate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        k%=len(nums)
+        nums[:]=nums[-k::]+nums[:-k:]
