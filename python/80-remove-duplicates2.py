411fec951908a4205c03ff94ec30742567261f30
sreya287 <sreyamerinsam2828@gmail.com>
2025-11-16 19:47:49 +0530

    Create 80-remove-duplicates2.py

---
+++ python/80-remove-duplicates2.py
@@ -0,0 +1,19 @@
+#removeduplicates from sorted array ii
+#difficulty: easy
+#language: python
+#link: https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii/
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if len(nums)<=2:
+            return len(nums)
+        k=2
+        for i in range(2,len(nums)):
+            if nums[i]!=nums[k-2]:
+                nums[k]=nums[i]
+                k+=1
+        
+        return k
