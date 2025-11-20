4fec80b7cb3d71e91a61407fcb636eb7c8a20641
sreya287 <sreyamerinsam2828@gmail.com>
2025-11-20 20:02:05 +0530

    Create 50-powx-n.py

---
+++ python/50-powx-n.py
@@ -0,0 +1,8 @@
+#pw(x,n)
+#difficulty: medium
+#language: python
+#link:https://leetcode.com/problems/powx-n/
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        return pow(x,n)
+        
